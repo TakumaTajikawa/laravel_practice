b5eea923935c6fe9fa22673c4c72ce4515551f19
TajikawaTakuma <tajikawa@arsaga.jp>
2021-08-01 23:25:40 +0900

    p128まで終了

--- .backend/app/Http/Controllers/HelloController.php
+++ .backend/app/Http/Controllers/HelloController.php
@@ -16,17 +16,7 @@ class HelloController extends Controller
 
     public function index()
     {
-        $data = ['msg' => '', 'data' => []];
-        $msg = 'get: ';
-        $result = [];
-        DB::table('peoples')->orderBy('name', 'asc')->chunk(2, function($items) use (&$msg, &$result) {
-            foreach ($items as $item) {
-                $msg .= $item->id . ':' . $item->name . '';
-                $result += array_merge($result, [$item]);
-                break;
-            }
-            return true;
-        });
+        
             
 
         $data = [
--- .backend/database/seeders/PeopleSeeder.php
+++ .backend/database/seeders/PeopleSeeder.php
@@ -16,19 +16,69 @@ class PeopleSeeder extends Seeder
     {
         $peoplesParams = [
             [
-                'name' => '山田太郎',
-                'email' => 'taro@yamdada.com',
-                'age' => '34',
+                'name' => 'syohei',
+                'email' => 'shohei@yamdada.com',
+                'age' => '27',
             ],
             [
-                'name' => '大島花子',
-                'email' => 'hanako@ohshima.com',
+                'name' => 'kenta',
+                'email' => 'kenta@ohshima.com',
                 'age' => '19',
             ],
             [
-                'name' => '神崎幸子',
-                'email' => 'sachiko@kanzaki.com',
+                'name' => 'yu',
+                'email' => 'yu@kanzaki.com',
                 'age' => '45',
+            ],
+            [
+                'name' => 'taeko',
+                'email' => 'taeko@kanzaki.com',
+                'age' => '23',
+            ],
+            [
+                'name' => 'kaede',
+                'email' => 'kaede@kanzaki.com',
+                'age' => '55',
+            ],
+            [
+                'name' => 'kohei',
+                'email' => 'kohei@kanzaki.com',
+                'age' => '24',
+            ],
+            [
+                'name' => 'masahiko',
+                'email' => 'masahiko@kanzaki.com',
+                'age' => '33',
+            ],
+            [
+                'name' => 'keiko',
+                'email' => 'keiko@kanzaki.com',
+                'age' => '18',
+            ],
+            [
+                'name' => 'yurika',
+                'email' => 'yurika@kanzaki.com',
+                'age' => '37',
+            ],
+            [
+                'name' => 'kouki',
+                'email' => 'kouki@kanzaki.com',
+                'age' => '30',
+            ],
+            [
+                'name' => 'arata',
+                'email' => 'arata@kanzaki.com',
+                'age' => '67',
+            ],
+            [
+                'name' => 'bunta',
+                'email' => 'bunta@kanzaki.com',
+                'age' => '19',
+            ],
+            [
+                'name' => 'chika',
+                'email' => 'chika@kanzaki.com',
+                'age' => '27',
             ],
         ];
         DB::table('peoples')->insert($peoplesParams);
